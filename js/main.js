19155d87941cc7ee1dcaa959c154771761803a07
Maryna Zemlianska <zemaruv@google.com>
2025-11-08 19:30:10 +0200

    Created new pages and content for them

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+const tabItem = document.querySelectorAll('.tabs_btn-item');
+const tabContent = document.querySelectorAll('.tabs_content-item');
+
+tabItem.forEach(item => {
+  item.addEventListener('click', event => {
+    const button = event.currentTarget.dataset.button;
+
+
+    tabItem.forEach(btn => btn.classList.remove('tabs_btn-item--active'));
+    tabContent.forEach(content => content.classList.remove('tabs_content-item--active'));
+
+
+    event.currentTarget.classList.add('tabs_btn-item--active');
+    document.getElementById(button).classList.add('tabs_content-item--active');
+  });
+});
+
+
+
 const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",

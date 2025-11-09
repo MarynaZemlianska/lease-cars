a3feee80b7cdc5dd738f284b81f0b3f387733cef
Maryna Zemlianska <zemaruv@google.com>
2025-11-09 22:16:59 +0200

    Finished adaptive version

--- js/main.js
+++ js/main.js
@@ -15,6 +15,12 @@ tabItem.forEach(item => {
   });
 });
 
+const menuBtn = document.querySelector('.menu_btn');
+const menu = document.querySelector('.menu_list');
+
+menuBtn.addEventListener('click',() => {
+  menu.classList.toggle('menu_list--active');
+});
 
 
 const swiper = new Swiper(".swiper", {
